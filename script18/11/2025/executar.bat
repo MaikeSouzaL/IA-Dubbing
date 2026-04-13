17be2176b84ad0ea7f904b1c22749499b230a5ac
MaikeSouzaL <maikesouzaleite@gmail.com>
2026-04-13 18:33:27 -0300

    Remove obsolete files and dependencies related to audio processing and translation scripts
    - Deleted requirements.txt as it is no longer needed.
    - Removed 'sincronizar_e_juntar copy.py' and 'sincronizar_e_juntar.py' scripts which handled audio synchronization and processing.
    - Eliminated 'traduzir_para_pt.py' and 'transcrever.py' scripts that were responsible for translation and transcription tasks.
    - Removed 'transcrever_whisper.py' which was used for transcription using Whisper model.

--- script18/11/2025/executar.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Ativando o ambiente virtual...
-call venv\Scripts\activate
-echo.
-echo Executando script de transcricao...
-python transcrever.py
-echo.
-echo Processo concluído.
-pause
